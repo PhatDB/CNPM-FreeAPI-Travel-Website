cb6c25307425ebf3ef46bda959a48298a5c7eba9
ChiNguyen <chinguyen29825@gmail.com>
2024-05-10 10:25:00 +0700

    add fetch API

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const PLACES_BASE_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
-const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/';
+const PLACES_BASE_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
 export const getCitiesData = async (inputValue) => {
     try {
@@ -9,12 +9,12 @@ export const getCitiesData = async (inputValue) => {
             data: { data },
         } = await axios(PLACES_BASE_URL, {
             params: {
-                minPopulation: '100000',
+                minPopulation: "100000",
                 namePrefix: inputValue,
             },
             headers: {
-                'X-RapidAPI-Key': import.meta.env.VITE_GEODB_API_KEY,
-                'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+                "X-RapidAPI-Key": import.meta.env.VITE_GEODB_API_KEY,
+                "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
             },
         });
         return data;
@@ -23,3 +23,15 @@ export const getCitiesData = async (inputValue) => {
     }
 };
 
+export const getCurrencyConvert = async (codeFromCurrency, codeToCurrency) => {
+    try {
+        const { data } = await axios(
+            `https://v6.exchangerate-api.com/v6/${
+                import.meta.env.VITE_EXCHANGERATE_API_KEY
+            }/pair/${codeFromCurrency}/${codeToCurrency}`
+        );
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
