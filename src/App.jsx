d1708a14eed2e0ddae07daf9177dbf8b4f0ce8eb
PhatDB <mrkujitv@gmail.com>
2024-05-25 14:54:44 +0700

    add logic places detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { Box, CssBaseline } from "@mui/material";
-import Header from "./components/Header/Header";
-import CurrencyConverter from "./components/CurrencyConverter/CurrencyConverter";
-import { useEffect, useState } from "react";
-import {
-    getCurrenWeatherData,
-    getDailyWeatherData,
-    getPlacesData,
-} from "./utils/fetchData";
-import Weather from "./components/Weather/Weather";
-import List from "./components/List/List";
-import Footer from "./components/Footer/Footer";
+import { Box, CssBaseline } from '@mui/material';
+import Header from './components/Header/Header';
+import CurrencyConverter from './components/CurrencyConverter/CurrencyConverter';
+import { useEffect, useState } from 'react';
+import { getCurrenWeatherData, getDailyWeatherData, getPlacesData } from './utils/fetchData';
+import Weather from './components/Weather/Weather';
+import List from './components/List/List';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
-    const [search, setSearch] = useState("");
+    const [search, setSearch] = useState('');
     const [coords, setCoords] = useState({ lat: 12.24507, lon: 109.19432 });
 
     const [currenWeather, setCurrenWeather] = useState([]);
@@ -20,11 +16,16 @@ const App = () => {
 
     const [places, setPlaces] = useState([]);
 
-    const [type, setType] = useState("restaurants");
-    const [rating, setRating] = useState("");
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
 
     const [isLoading, setIsLoading] = useState(false);
     const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+    useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+        setFilteredPlaces(filteredPlaces);
+    }, [rating]);
 
     useEffect(() => {
         getCurrenWeatherData(coords).then((data) => {
@@ -35,15 +36,20 @@ const App = () => {
         });
     }, [coords]);
 
+    useEffect(() => {
+        setIsLoading(true);
+        getPlacesData(coords, type).then((data) => {
+            setPlaces(data);
+            setFilteredPlaces([]);
+            setIsLoading(false);
+        });
+    }, [coords, type]);
+
     return (
         <>
             <CssBaseline />
-            <Box sx={{ background: "#DEF1FF" }}>
-                <Header
-                    search={search}
-                    setSearch={setSearch}
-                    setCoords={setCoords}
-                />
+            <Box sx={{ background: '#DEF1FF' }}>
+                <Header search={search} setSearch={setSearch} setCoords={setCoords} />
                 <CurrencyConverter />
                 <Weather currenData={currenWeather} dailyData={dailyWeather} />
                 <List
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
-const PLACES_BASE_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
-const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/";
+const PLACES_BASE_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
 export const getCitiesData = async (inputValue) => {
     try {
@@ -9,12 +9,12 @@ export const getCitiesData = async (inputValue) => {
             data: { data },
         } = await axios(PLACES_BASE_URL, {
             params: {
-                minPopulation: "100000",
+                minPopulation: '100000',
                 namePrefix: inputValue,
             },
             headers: {
-                "X-RapidAPI-Key": import.meta.env.VITE_GEODB_API_KEY,
-                "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+                'X-RapidAPI-Key': import.meta.env.VITE_GEODB_API_KEY,
+                'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
             },
         });
         return data;
@@ -26,9 +26,9 @@ export const getCitiesData = async (inputValue) => {
 export const getCurrenWeatherData = async (coords) => {
     try {
         const { data } = await axios(
-            `${WEATHER_BASE_URL}weather?lat=${coords.lat}&lon=${
-                coords.lon
-            }&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`
+            `${WEATHER_BASE_URL}weather?lat=${coords.lat}&lon=${coords.lon}&appid=${
+                import.meta.env.VITE_WEATHER_API_KEY
+            }&units=metric`,
         );
         return data;
     } catch (error) {
@@ -39,9 +39,9 @@ export const getCurrenWeatherData = async (coords) => {
 export const getDailyWeatherData = async (coords) => {
     try {
         const { data } = await axios(
-            `${WEATHER_BASE_URL}forecast?lat=${coords.lat}&lon=${
-                coords.lon
-            }&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`
+            `${WEATHER_BASE_URL}forecast?lat=${coords.lat}&lon=${coords.lon}&appid=${
+                import.meta.env.VITE_WEATHER_API_KEY
+            }&units=metric`,
         );
         return data;
     } catch (error) {
@@ -53,21 +53,17 @@ export const getPlacesData = async (coords, type) => {
     try {
         const {
             data: { data },
-        } = await axios(
-            `https://travel-advisor.p.rapidapi.com/${type}/list-by-latlng`,
-            {
-                params: {
-                    latitude: coords.lat,
-                    longitude: coords.lon,
-                    limit: "30",
-                },
-                headers: {
-                    "X-RapidAPI-Key": import.meta.env
-                        .VITE_TRAVEL_ADVISOR_API_KEY,
-                    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-                },
-            }
-        );
+        } = await axios(`https://travel-advisor.p.rapidapi.com/${type}/list-by-latlng`, {
+            params: {
+                latitude: coords.lat,
+                longitude: coords.lon,
+                limit: '30',
+            },
+            headers: {
+                'X-RapidAPI-Key': import.meta.env.VITE_TRAVEL_ADVISOR_API_KEY,
+                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+            },
+        });
         return data;
     } catch (error) {
         console.log(error);
@@ -79,7 +75,7 @@ export const getCurrencyConvert = async (codeFromCurrency, codeToCurrency) => {
         const { data } = await axios(
             `https://v6.exchangerate-api.com/v6/${
                 import.meta.env.VITE_EXCHANGERATE_API_KEY
-            }/pair/${codeFromCurrency}/${codeToCurrency}`
+            }/pair/${codeFromCurrency}/${codeToCurrency}`,
         );
         return data;
     } catch (error) {
